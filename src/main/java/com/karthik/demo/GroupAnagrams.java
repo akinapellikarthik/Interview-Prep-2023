78158dd9c82540c1e5fb35e0ab566b3724cc1fc6
akinapellikarthik <karthik.akinapelli@gmail.com>
2024-01-12 17:14:37 +0530

    leetcode sols

---
+++ src/main/java/com/karthik/demo/GroupAnagrams.java
@@ -0,0 +1,32 @@
+package com.karthik.demo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+
+  public List<List<String>> groupAnagrams(String[] strs) {
+    Map<String, List<String>> map = new HashMap<>();
+    List<List<String>> result = new ArrayList<>();
+    for(String s: strs){
+      var sCharArray = s.toCharArray();
+      Arrays.sort(sCharArray);
+      String s1 = new String(sCharArray);
+      if(!map.containsKey(s1)){
+        List<String> arr = new ArrayList<>();
+        arr.add(s);
+        map.put(s1,arr);
+      }
+      else {
+        map.get(s1).add(s);
+      }
+
+    }
+    result.addAll(map.values());
+    return result;
+  }
+
+}
---
+++ src/main/java/com/karthik/demo/removeStars.java
@@ -0,0 +1,23 @@
+package com.karthik.demo;
+
+import java.util.Stack;
+
+public class removeStars {
+
+  public String removeStars(String s) {
+    Stack<Character> tempStack = new Stack<>();
+    StringBuffer sb = new StringBuffer();
+    for(Character ch: s.toCharArray()){
+      if(ch != '*')
+        tempStack.push(ch);
+      else {
+        tempStack.pop();
+      }
+    }
+
+    while(!tempStack.isEmpty()){
+      sb.append(tempStack.pop());
+    }
+    return sb.reverse().toString();
+  }
+}
