9882fbf1af1bc00b962e37e7e5b6540f73c20820
akinapellikarthik <karthik.akinapelli@gmail.com>
2024-05-20 19:47:47 +0530

    https://leetcode.com/problems/majority-element/

---
+++ src/main/java/com/karthik/demo/problems/MajorityElement.java
@@ -0,0 +1,24 @@
+package com.karthik.demo.problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement {
+
+    //https://leetcode.com/problems/majority-element/
+    public int majorityElement(int[] nums) {
+        int searchCnt = ( nums.length / 2 ) + 1;
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for(int i: nums) {
+            countMap.put(i, countMap.getOrDefault(i, 0) + 1);
+        }
+
+        var ele = countMap.entrySet().stream().filter(s -> s.getValue() >= searchCnt)
+                .findFirst();
+
+        if(ele.isEmpty())
+            return -1;
+        else
+            return ele.get().getKey();
+    }
+}
