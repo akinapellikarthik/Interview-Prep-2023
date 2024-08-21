4c4afba5412dd7537b5e40c277908975a3fb53db
akinapellikarthik <karthik.akinapelli@gmail.com>
2024-08-21 22:37:53 +0530

    Q implementation

---
+++ src/main/java/com/karthik/demo/dsa2k24/datastructures/queue/Main.java
@@ -0,0 +1,15 @@
+package com.karthik.demo.dsa2k24.datastructures.queue;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Queue queue = new Queue(10);
+        queue.enqueue(11);
+        queue.enqueue(12);
+        queue.enqueue(13);
+        queue.enqueue(14);
+        queue.enqueue(15);
+
+        queue.printQueue();
+    }
+}
