e69fd105b8282e2b8f00f99f6d382ee99bc53cd8
akinapellikarthik <karthik.akinapelli@gmail.com>
2024-08-21 22:59:26 +0530

    formatting changes checkin

--- src/main/java/com/karthik/demo/dsa2k24/datastructures/queue/Main.java
+++ src/main/java/com/karthik/demo/dsa2k24/datastructures/queue/Main.java
@@ -2,24 +2,24 @@ package com.karthik.demo.dsa2k24.datastructures.queue;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Queue queue = new Queue(10);
-        queue.enqueue(11);
-        queue.enqueue(12);
-        queue.enqueue(13);
-        queue.enqueue(14);
-        queue.enqueue(15);
+  public static void main(String[] args) {
+    Queue queue = new Queue(10);
+    queue.enqueue(11);
+    queue.enqueue(12);
+    queue.enqueue(13);
+    queue.enqueue(14);
+    queue.enqueue(15);
 
-        queue.printQueue();
-        System.out.println("Dequeueing.....");
-        System.out.println(queue.dequeue().value);
-        System.out.println(queue.dequeue().value);
-        System.out.println(queue.dequeue().value);
-        System.out.println(queue.dequeue().value);
-        System.out.println(queue.dequeue().value);
-        System.out.println(queue.dequeue().value);
-        System.out.println(queue.dequeue());
+    queue.printQueue();
+    System.out.println("Dequeueing.....");
+    System.out.println(queue.dequeue().value);
+    System.out.println(queue.dequeue().value);
+    System.out.println(queue.dequeue().value);
+    System.out.println(queue.dequeue().value);
+    System.out.println(queue.dequeue().value);
+    System.out.println(queue.dequeue().value);
+    System.out.println(queue.dequeue());
 
-        queue.printQueue();
-    }
+    queue.printQueue();
+  }
 }
