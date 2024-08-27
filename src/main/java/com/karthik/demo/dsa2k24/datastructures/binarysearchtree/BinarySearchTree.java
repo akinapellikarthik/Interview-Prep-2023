33c4f1d141718528a113c78d83dfecbafa309584
akinapellikarthik <karthik.akinapelli@gmail.com>
2024-08-27 22:56:11 +0530

    formmated files

--- src/main/java/com/karthik/demo/dsa2k24/datastructures/binarysearchtree/BinarySearchTree.java
+++ src/main/java/com/karthik/demo/dsa2k24/datastructures/binarysearchtree/BinarySearchTree.java
@@ -2,66 +2,64 @@ package com.karthik.demo.dsa2k24.datastructures.binarysearchtree;
 
 public class BinarySearchTree {
 
-    class Node {
-        int value;
-        Node left;
-        Node right;
-        public Node(int value) {
-            this.value = value;
-        }
+  class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value) {
+      this.value = value;
+    }
+  }
+
+  Node root;
+
+  public BinarySearchTree() {
+    root = null;
+  }
+
+  public boolean insert(int value) {
+    Node newNode = new Node(value);
+    if (root == null) {
+      root = newNode;
+      return true;
     }
 
-     Node root;
-
-    public BinarySearchTree() {
-        root = null;
-    }
-
-    public boolean insert(int value) {
-        Node newNode = new Node(value);
-        if(root == null){
-            root = newNode;
-            return true;
-        }
-
-        Node temp = root;
-        while(true){
-            if(temp.value == newNode.value){
-                return false;
-            }
-            if(newNode.value < temp.value){
-                if(temp.left == null){
-                    temp.left = newNode;
-                    return true;
-                }
-                temp = temp.left;
-            }else {
-                if(temp.right == null){
-                    temp.right = newNode;
-                    return true;
-                }
-                temp = temp.right;
-            }
-        }
-
-    }
-
-    public boolean contains(int value) {
-        if(root == null)
-            return false;
-        Node temp = root;
-        while(temp != null){
-            if(temp.value == value){
-                return true;
-            }else if(temp.value < value){
-                //left traversal
-                temp = temp.left;
-            }
-            else {
-                //right traversal
-                temp = temp.right;
-            }
-        }
+    Node temp = root;
+    while (true) {
+      if (temp.value == newNode.value) {
         return false;
+      }
+      if (newNode.value < temp.value) {
+        if (temp.left == null) {
+          temp.left = newNode;
+          return true;
+        }
+        temp = temp.left;
+      } else {
+        if (temp.right == null) {
+          temp.right = newNode;
+          return true;
+        }
+        temp = temp.right;
+      }
     }
+  }
+
+  public boolean contains(int value) {
+    if (root == null) return false;
+    Node temp = root;
+    while (temp != null) {
+      if (temp.value == value) {
+        return true;
+      } else if (temp.value < value) {
+        // left traversal
+        temp = temp.left;
+      } else {
+        // right traversal
+        temp = temp.right;
+      }
+    }
+    return false;
+  }
 }
